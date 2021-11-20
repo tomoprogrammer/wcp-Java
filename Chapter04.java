f7d2587d91122d36636307ec55cbf57dac3062eb
tomoprogrammer <train.vegacat@gmail.com>
2021-11-20 17:03:51 +0900

    like

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    final int LUCKY_NUMBER = 5;
+    String str = "ともプログラマー";
+    System.out.println(str);
+    str = "二度あることは三度ある";
+    System.out.println(str);
+    System.out.println(LUCKY_NUMBER);
+  }
+}
