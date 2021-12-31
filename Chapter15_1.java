37caacb6a4cb001b258da54281e86131afb511d6
tomoprogrammer <train.vegacat@gmail.com>
2021-12-31 14:50:38 +0900

    chapter15

--- Chapter15_1.java
+++ Chapter15_1.java
@@ -1,4 +1,4 @@
-package practice;
+
 
 public class Chapter15_1 {
     public static void main(String[] args) {
--- Chapter15_2.java
+++ Chapter15_2.java
@@ -1,4 +1,3 @@
-package practice;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
